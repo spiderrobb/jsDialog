2644e4ed39e5160d0dac0752fedc1cfc9a15c32c
Robbin Harris <spiderrobb@gmail.com>
2013-04-16 22:09:03 -0700

    Custom Styling
    Added ability to add custom styling to any element that makes up the
    jsDialog

--- jsDialog.js
+++ jsDialog.js
@@ -10,7 +10,7 @@ function jsDialog(args){
 	// getting dialog title
 	var title = args['title'] || '';
 	// getting dialog style
-	var style = args['style'] || 'default';
+	var theme = args['theme'] || 'default';
 	// getting width of dialog
 	var width = args['width'] || '800px';
 	// getting height of dialog
@@ -39,8 +39,19 @@ function jsDialog(args){
 		content:    document.createElement('div')
 	};
 	
-	// building close function
+	// applying any custom style
 	var dialog = this;
+	if (typeof args['style'] != 'undefined') {
+		Object.keys(this.elements).forEach(function(element){
+			if (typeof args['style'][element] != 'undefined') {
+				Object.keys(args['style'][element]).forEach(function(style){
+					dialog.elements[element].style[style] = args['style'][element][style];
+				});
+			}
+		});
+	}
+	
+	// building close function
 	this.close = function(e) {
 		// checking if the correct element has been clicked
 		if (!e
@@ -66,40 +77,40 @@ function jsDialog(args){
 	// building dialog shadow
 	var shadow = this.elements.shadow;
 	shadow.id = 'jsDialog_shadow_'+jsDialog.count;
-	shadow.className = 'jsDialog_shadow-'+style;
+	shadow.className = 'jsDialog_shadow-'+theme;
 	shadow.onclick = this.close;
 	document.body.appendChild(shadow);
 	
 	// building dialog container
 	var container = this.elements.container;
 	container.id = 'jsDialog_container_'+jsDialog.count;
-	container.className = 'jsDialog_container-'+style;
+	container.className = 'jsDialog_container-'+theme;
 	container.style.width = width;
 	shadow.appendChild(container);
 	
 	// building dialog title bar
 	var title_bar = this.elements.title_bar;
 	title_bar.id = 'jsDialog_titlebar_'+jsDialog.count;
-	title_bar.className = 'jsDialog_titlebar-'+style;
+	title_bar.className = 'jsDialog_titlebar-'+theme;
 	container.appendChild(title_bar);
 	
 	// building dialog title text
 	var title_text = this.elements.title_text;
 	title_text.id = 'jsDialog_titletext_'+jsDialog.count;
-	title_text.className = 'jsDialog_titletext-'+style;
+	title_text.className = 'jsDialog_titletext-'+theme;
 	title_text.innerHTML = title;
 	title_bar.appendChild(title_text);
 
 	// building close button for dialog
 	var close = this.elements.close;
 	close.id = 'jsDialog_close_'+jsDialog.count;
-	close.className = 'jsDialog_close-'+style;
+	close.className = 'jsDialog_close-'+theme;
 	title_bar.appendChild(close);
 
 	// building content
 	var content = this.elements.content;
 	content.id = 'jsDialog_content_'+jsDialog.count;
-	content.className = 'jsDialog_content-'+style;
+	content.className = 'jsDialog_content-'+theme;
 	// setting content of dialog
 	if (typeof args['content'] == 'object') {
 		content.appendChild(args['content']);
